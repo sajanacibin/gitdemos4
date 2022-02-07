49a30b5ba66294c6725ccad305200206471228a5
sajanacibin <sajana.cibin@gmail.com>
2022-02-07 19:08:03 +0530

    new pjt

---
+++ src/testcases/testcase.java
@@ -0,0 +1,27 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import objectrepositries.homepage;
+
+public class testcase {
+	@Test
+	
+	public void tests()
+	
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/JP/Desktop/chromedriver");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		homepage hp=new homepage(driver);
+		hp.username().sendKeys("hello");
+		hp.Password().sendKeys("password");
+		hp.ButtonClick().click();
+		
+		
+		
+	}
+
+}
